e0c68114da1e77f812d6aebedf6bdbc3ec5af567
atomicbolts 3 <154646419+mariocraft987@users.noreply.github.com>
2025-06-13 09:58:29 -0700

    test idk if this will work IDK HOW ZELOS WORK

--- blockly_editor/scripts/custom_category_es6.js
+++ blockly_editor/scripts/custom_category_es6.js
@@ -37,7 +37,27 @@ class CustomCategory extends Blockly.ToolboxCategory {
      */
     setSelected(isSelected) {
       // We do not store the label span on the category, so use getElementsByClassName.
-
+      const labelDom = this.rowDiv_.getElementsByClassName('blocklyToolboxCategoryContainer')[0];
+      if (isSelected) {
+        // Change the background color of the div to white.
+        this.rowDiv_.style.backgroundColor = 'white';
+        // Set the colour of the text to the colour of the category.
+        labelDom.style.color = this.colour_;
+        this.iconDom_.style.color = this.colour_;
+      } else {
+        // Set the background back to the original colour.
+        this.rowDiv_.style.backgroundColor = this.colour_;
+        // Set the text back to white.
+        labelDom.style.color = 'white';
+        this.iconDom_.style.color = 'white';
+      }
+      
+      // This is used for accessibility purposes.
+      Blockly.utils.aria.setState(
+        /** @type {!Element} */ (this.htmlDiv_),
+        Blockly.utils.aria.State.SELECTED,
+        isSelected,
+      );
 
     }
   
