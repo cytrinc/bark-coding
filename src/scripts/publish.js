4c3079785855013f5730aebebcabac172e5fbcc1
atomicbolts <154646419+mariocraft987@users.noreply.github.com>
2025-06-29 12:37:00 -0700

    yay

--- src/scripts/publish.js
+++ src/scripts/publish.js
@@ -1,14 +1,6 @@
-function getprojectcontent() {
-    var file = document.getElementById("fileForUpload").files[0];
-    if (file) {
-        var reader = new FileReader();
-        reader.readAsText(file, "UTF-8");
-        reader.onload = function (evt) {
-            return evt.target.result;
-        }
-        reader.onerror = function (evt) {
-            return "error reading file";
-        }
+async function getprojectcontent(event) {
+  const file = event.target.files.item(0)
+  const text = await file.text();
+  
+  return text;
 }
-}
-
