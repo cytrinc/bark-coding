cf8e1a396b66188b8610bd4dccac6939fa567fe5
atomicbolts <154646419+mariocraft987@users.noreply.github.com>
2025-06-29 12:34:26 -0700

    shit damnit

--- src/scripts/publish.js
+++ src/scripts/publish.js
@@ -12,20 +12,3 @@ function getprojectcontent() {
 }
 }
 
-  document.getElementById("publishbutton3427408932750943703827").addEventListener('click', async () => {
-    const postcommentresponse = await fetch(`https://api.bark.dumorando.com/api/v2/publish?token=${encodeURIComponent(token)}&title=${encodeURIComponent(document.getElementById("title").value)}&description=${encodeURIComponent(document.getElementById("description").value)}`, {
-      method: 'POST',
-      headers: { 
-        'Content-type': 'application/json'
-      },  
-    });
-    if (postcommentresponse.status == 400) {
-      document.getElementById("commenterror").innerHTML = "400: Invalid Request!";
-    }
-    else if (postcommentresponse.status == 404) {
-      document.getElementById("commenterror").innerHTML = "404: Account not found!";
-    } else {
-	location.reload();
-    }
-  });
-  
