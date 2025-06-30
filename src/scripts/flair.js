449a7db1579e54514702e6711ee56b9ffcbdd5b3
atomicbolts <154646419+mariocraft987@users.noreply.github.com>
2025-06-30 11:31:21 -0700

    i do know spans dont go inside of divs or the other way around.....

---
+++ src/scripts/flair.js
@@ -0,0 +1,11 @@
+document.getElementById("flair").innerHTML += `
+<div class="flair">
+<div class="near"><img src="/src/images/assets/flair/earth.gif" width="40" alt="a spinning earth"></div>
+<div class="inner">This user lives on earth!</div>
+</div>`
+
+document.getElementById("flair").innerHTML += `
+<div class="flair">
+<div class="near" style="background-color: #fcba03;"><img src="https://neo-neighborhoods.neocities.org/SiliconValley/6603/clown.gif" width="40" alt="a clown"></div>
+<div class="inner" style="background-color: #fcba03;">This user is funny!</div>
+</div>`
