e96acc2b35df8ee9ef921de6d1facd81ffbc8a33
atomicbolts <154646419+mariocraft987@users.noreply.github.com>
2025-07-29 16:06:11 -0700

    wtf

--- blockly_editor/advanced_editor.js
+++ blockly_editor/advanced_editor.js
@@ -1,5 +1,5 @@
 function advanced_editor() {
-    document.getElementById("code").innerHTML = `
+    document.getElementById("blocklyDiv").innerHTML = `
     <textarea id="code" spellcheck="false">let dog = new sprite("dog", 0, 0, 100); <br/>dog.draw();<br/><br/>// draws a dog on the stage</div>
     </textarea>`
 }
