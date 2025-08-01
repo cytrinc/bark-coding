fffb8884a414ba6f9ac96012b6eb8c803a0a72d2
atomicbolts <154646419+mariocraft987@users.noreply.github.com>
2025-07-31 18:25:13 -0700

    width="350"

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ This may take some time as i am very busy this season.
 <br/>
 ALSO IM ADDING THE ADVANCED EDITOR TO THE BLOCKLY EDITOR!!!
 <br/>
-<img src="https://i.ibb.co/VWt8q6r7/Screenshot-2025-07-31-181122.png" width="750" alt="the blockly editor site having text to code support.">
+<img src="https://i.ibb.co/VWt8q6r7/Screenshot-2025-07-31-181122.png" width="350" alt="the blockly editor site having text to code support.">
 `;
 
 let baseUrl = window.location.origin;
